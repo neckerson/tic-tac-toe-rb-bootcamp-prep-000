401269be273171a6f33f0f084e188bbd88b8ad49
Nate Eckerson <nate.eckerson@gmail.com>
2018-06-15 15:22:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,6 +11,24 @@ WIN_COMBINATIONS = [
   [6,4,2]
 ]
 
+
+# Define display_board that accepts a board and prints
+# out the current state.
+
+def display_board(board, board_array = [])
+  e = 0
+  3.times do |i|
+  board_array << " #{board[e]} | #{board[e+1]} | #{board[e+2]} "
+    if i/2 == 0
+      board_array << "-----------\n"
+    end
+  e += 3  
+  end
+  board_array.each do |x|
+    puts x
+  end
+end
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
